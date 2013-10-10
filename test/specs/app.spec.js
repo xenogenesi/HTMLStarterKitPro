44126b84fe26b6ec92ee1876e2276d770b3709cf
RainerAtSpirit <R.Wittmann@spirit.de>
2013-10-10 22:00:15 +0200

    add runs waitsFor for async testing of main.js

--- test/specs/app.spec.js
+++ test/specs/app.spec.js
@@ -11,29 +11,37 @@ describe('applicationhost', function() {
     });
 });
 
-    // Ready to start the real tests#
-    define([ 'plugins/router', 'main'], function(router) {
+// Ready to start the real tests#
+define([ 'plugins/router', 'durandal/system', 'main'], function( router, system ) {
+    "use strict";
 
-        describe('main', function() {
-            it('should trigger a "router:navigation:complete" event', function() {
-                var ready = false,
-                    result;
+    describe('main', function() {
+        it('should trigger a "router:navigation:complete" event with two arguments', function() {
+            var ready = false,
+                result;
 
-                runs(function() {
-                    router.on('router:navigation:complete').then(function() {
-                        result = true;
-                        ready = true;
-                    });
-                });
-
-                waitsFor(function() {
-                    return result;
-                });
-
-                runs(function() {
-                    expect(result).toBeTruthy();
+            runs(function() {
+                router.on('router:navigation:complete').then(function() {
+                    result = arguments;
+                    ready = true;
                 });
             });
+
+            waitsFor(function() {
+                return result;
+            });
+
+            runs(function() {
+                system.log('runs', result);
+                expect(result.length).toBeTruthy(2);
+            });
+        });
+
+        it('after "router:navigation:complete" "viewmodels/shell" should be loaded', function() {
+            var shell = require('viewmodels/shell');
+
+            expect(shell.__moduleId__).toBeTruthy('viewmodels/shell');
         });
     });
+});
 
